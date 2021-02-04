9520bf9083aceb1f4ab94df20384c6f033d3d812
Gabriela E Avalos <gabrielaeavalos@gabrielas-air.home>
2021-02-04 00:22:47 -0600

    Retirement Eligibility query completed

---
+++ queries.sql
@@ -0,0 +1,12 @@
+--RETIREMENT ELIGIBILITY
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--RETIREMENT ELIGIBILITY INTO TABLE
+SELECT first_name, last_name
+INTO retirement_info --new table created
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
